56a7ae256c67f13376bf3e389d7fbb45b244f1c0
Boniface Mwenda <boniface.mwenda@andela.com>
2019-05-27 13:39:33 +0300

    Tail insertion

--- data-structures/doubly_linked_list.rb
+++ data-structures/doubly_linked_list.rb
@@ -33,4 +33,39 @@ class DoublyLinkedList
     head.previous_node = new_node
     self.head = new_node
   end
+
+  # Inserting new node after a given node
+  def after_node_insertion(previous_node, new_data)
+    return unless previous_node
+
+    new_node = Node.new(new_data)
+
+    new_node.previous_node = previous_node
+    new_node.next_node = previous_node.next_node
+
+    previous_node.next_node = new_node
+    new_node.next_node.previous_node = new_node if new_node.next_node
+  end
+
+  # Inserting new node at the end of the list
+  def tail_insertion(value)
+    current = head
+    new_node = Node.new(value)
+
+    while !current.next_node.nil?
+      current = current.next_node
+    end
+    new_node.previous_node = current
+    current.next_node = new_node
+  end
+
+  def items
+    current = head
+    elements = []
+    while !current.next_node.nil?
+      elements << current
+      current = current.next_node
+    end
+    elements << current
+  end
 end
